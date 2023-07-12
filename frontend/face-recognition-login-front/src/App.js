9d7b192b3a3eac2fdde1988bf047424e37e0728d
Arya Das <arya.das512ht@gmail.com>
2023-07-13 01:19:48 +0530

    first commit

---
+++ frontend/face-recognition-login-front/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Facelogin from "./Facelogin";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <Facelogin />
+    </div>
+  );
+}
+
+export default App;
